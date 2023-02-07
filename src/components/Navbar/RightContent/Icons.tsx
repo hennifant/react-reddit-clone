64e6b6f65696ce344ed47e4376a3c171401cf17d
hennifant <henning.schwichow@gmx.de>
2023-02-07 17:01:43 +0100

    Added navbar icons

---
+++ src/components/Navbar/RightContent/Icons.tsx
@@ -0,0 +1,21 @@
+import { IconProps } from "@chakra-ui/react";
+import React from "react";
+import { Box, Flex, Icon } from "@chakra-ui/react";
+import { BsArrowUpRightCircle, BsChatDots } from "react-icons/bs";
+import { GrAdd } from "react-icons/gr";
+import {
+  IoFilterCircleOutline,
+  IoNotificationsOutline,
+  IoVideocamOutline,
+} from "react-icons/io5";
+
+const Icons: React.FC = () => {
+  return (
+    <Flex>
+      <Flex display={{ base: "none", md: "flex" }}>icons</Flex>
+      <></>
+    </Flex>
+  );
+};
+
+export default Icons;

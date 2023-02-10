633344252cd9ee5d4690ecc11765ce8a04d76a2b
hennifant <henning.schwichow@gmx.de>
2023-02-10 09:58:05 +0100

    Added community directory in navbar

---
+++ src/components/Navbar/Directory/Directory.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Flex,
+  Icon,
+  Menu,
+  MenuButton,
+  MenuList,
+  Text,
+  Image,
+} from "@chakra-ui/react";
+import { TiHome } from "react-icons/ti";
+
+const UserMenu: React.FC = () => {
+  return (
+    <Menu>
+      <>
+        <MenuButton
+          color="Black"
+          cursor="pointer"
+          padding="0px 6px"
+          borderRadius="4px"
+          _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
+          mr={2}
+          ml={{ base: 0, md: 2 }}
+        >
+          <Flex
+            alignItems="center"
+            justifyContent="space-between"
+            width={{ base: "auto", lg: "200px" }}
+          >
+            <Flex alignItems="center" bg="white">
+              <Icon fontSize={24} mr={{ base: 1, md: 2 }} as={TiHome} />
+              <Flex
+                display={{ base: "none", lg: "flex" }}
+                flexDirection="column"
+                fontSize="10pt"
+                bg="white"
+              >
+                <Text fontWeight={600} fontSize="10pt">
+                  Home
+                </Text>
+              </Flex>
+            </Flex>
+            <ChevronDownIcon color="gray.500" />
+          </Flex>
+        </MenuButton>
+        <MenuList
+          maxHeight="300px"
+          overflow="scroll"
+          overflowX="hidden"
+        ></MenuList>
+      </>
+    </Menu>
+  );
+};
+export default UserMenu;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,12 +4,22 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase/clientApp";
 import SearchInput from "./SearchInput";
 import RightContent from "./RightContent/RightContent";
+import Directory from "./Directory/Directory";
 
 const Navbar: React.FC = () => {
   const [user, loading, error] = useAuthState(auth);
   return (
-    <Flex bg="white" height="44px" padding="6px 12px">
-      <Flex align="center">
+    <Flex
+      bg="white"
+      height="44px"
+      padding="6px 12px"
+      justify={{ md: "space-between" }}
+    >
+      <Flex
+        align="center"
+        width={{ base: "40px", md: "auto" }}
+        mr={{ base: 0, md: "auto" }}
+      >
         <Image alt="Reddit Face" src="/images/redditFace.svg" height="30px" />
         <Image
           alt="Reddit Text"
@@ -18,8 +28,8 @@ const Navbar: React.FC = () => {
           display={{ base: "none", md: "unset" }}
         />
       </Flex>
-      {/*<Directory />*/}
-      <SearchInput />
+      {user && <Directory />}
+      <SearchInput user={user} />
       <RightContent user={user} />
     </Flex>
   );

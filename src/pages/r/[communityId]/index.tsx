ec7128bf3900a912fbb26448702aa28e7d293392
hennifant <henning.schwichow@gmx.de>
2023-02-14 14:24:18 +0100

    Added community not found

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -4,11 +4,16 @@ import { firestore } from "../../../firebase/clientApp";
 import { doc, getDoc } from "firebase/firestore";
 import { Community } from "../../../atoms/communitiesAtom";
 import safeJsonStringify from "safe-json-stringify";
+import NotFound from "../../../components/Community/NotFound";
 
 type CommunityPageProps = { communityData: Community };
 
 const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
   console.log("here is data", communityData);
+
+  if (!communityData) {
+    return <NotFound />;
+  }
   return <div>Community Page{communityData.id}</div>;
 };
 
@@ -22,9 +27,11 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     const communityDoc = await getDoc(communityDocRef);
     return {
       props: {
-        communityData: JSON.parse(
-          safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() }) // needed for dates
-        ),
+        communityData: communityDoc.exists()
+          ? JSON.parse(
+              safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() }) // needed for dates
+            )
+          : "",
       },
     };
   } catch (error) {

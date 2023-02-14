8b039b2cae9117c5769fcc876eaf246166668aba
hennifant <henning.schwichow@gmx.de>
2023-02-14 16:40:35 +0100

    useCommunityData custom hook

--- src/components/Community/Header.tsx
+++ src/components/Community/Header.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import { Community } from "../../atoms/communitiesAtom";
 import { Flex, Box, Icon, Image, Text, Button } from "@chakra-ui/react";
 import { FaReddit } from "react-icons/fa";
+import useCommunityData from "../../hooks/useCommunityData";
 
 type HeaderProps = {
   communityData: Community;
 };
 
 const Header: React.FC<HeaderProps> = ({ communityData }) => {
-  const isJoined = false;
+  const { communityStateValue, onJoinOrLeaveCommunity } = useCommunityData();
+  const isJoined = !!communityStateValue.mySnippets.find(
+    (item) => item.communityId === communityData.id
+  );
+
   return (
     <Flex direction="column" width="100%" height="146px">
       <Box height="50%" bg="blue.400" />
@@ -50,7 +55,7 @@ const Header: React.FC<HeaderProps> = ({ communityData }) => {
                 height="30px"
                 pr={6}
                 pl={6}
-                onClick={() => {}}
+                onClick={() => onJoinOrLeaveCommunity(communityData, isJoined)}
               >
                 {isJoined ? "Joined" : "Join"}
               </Button>
